3d77efccc74f04309a9b0abc7d92a89e05167105
AllergySnipe <ritviks001@gmail.com>
2024-06-21 18:46:10 +0900

    Fixed update product api

--- product-service/migrations/versions/5f55e8f71cea_.py
+++ product-service/migrations/versions/5f55e8f71cea_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 52cc8df27546
+Revision ID: 5f55e8f71cea
 Revises: 
-Create Date: 2024-06-21 12:17:37.370603
+Create Date: 2024-06-21 17:12:22.636605
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '52cc8df27546'
+revision = '5f55e8f71cea'
 down_revision = None
 branch_labels = None
 depends_on = None
